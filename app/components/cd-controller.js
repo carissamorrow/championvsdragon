829390c4fa8fa135a0aabf88c5e4dffa1b99c549
carissa morrow <carissamorrow@hotmail.com>
2018-11-15 13:56:29 -0700

    fixingerrors

--- app/components/cd-controller.js
+++ app/components/cd-controller.js
@@ -2,8 +2,39 @@ import cdService from "./cd-service.js"
 
 let _cd = new cdService()
 
+function drawChampions(championArr) {
+  let template = ''
+  championArr.forEach(champion => {
+    let button = `
+    <button class="btn btn-success" onclick="app.controllers.cdController.addChampion(${champion.name})">Add</button>
+    `
+    template += champion.getChampionCard(button)
+  });
+  document.getElementById("champions").innerHTML = template
+}
+
+function drawDragons(dragonArr) {
+  let template = ''
+  dragonArr.forEach(dragon => {
+    let button = `
+    <button class="btn btn-success" onclick="app.controllers.cdController.addDragon()${dragon.name})">Add</button>
+  `
+    template += dragon.getDragonCard(button)
+  });
+  document.getElementById("dragons").innerHTML = template
+}
+
 export default class cdController {
   constructor() {
     console.log("cd controller")
+    _cd.getChampions(drawChampions)
+    _cd.getDragons(drawDragons)
+  }
+  addChampion() {
+
+  }
+
+  addDragons() {
+
   }
 }
--- app/components/cd-service.js
+++ app/components/cd-service.js
@@ -1,10 +1,29 @@
 import Dragon from "../models/dragon.js";
 import Champion from "../models/champion.js";
 
+// @ts-ignore
+let _dragonAPI = axios.create({
+  baseURL: 'https://dragon-duel.herokuapp.com/api/dragons'
+})
 
+// @ts-ignore
+let _championAPI = axios.create({
+  baseURL: 'https://dragon-duel.herokuapp.com/api/champions'
+})
+
+let dragons = []
+let champion = []
 
 export default class cdService {
   constructor() {
     console.log("cd service")
+  }
+
+  get dragons() {
+    return _dragonAPI
+  }
+
+  get champions() {
+    return _championAPI
   }
 }
